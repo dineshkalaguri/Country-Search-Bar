77cb2f32c171d5bdd2647b0150f9361b3868b2a0
dineshkalaguri <dineshkalaguri@gmail.com>
2024-09-14 16:33:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import SearchBar from './Components/searchBar';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Country Search Bar</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
